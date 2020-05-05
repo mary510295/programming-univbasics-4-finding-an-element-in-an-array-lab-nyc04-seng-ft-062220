94617f27e334a6bb9077b2d458cc0956698c8115
Fu <fu.goto@gmail.com>
2020-05-05 00:51:05 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
   i = 0
   while i < array.length do
-    if array[i] == value_to_find return i;
-     else i += 1
+    if array[i] == value_to_find return i
+    i += 1
     end
     nil
 end
